c16a3cca8560531944d2035fe73c12ec3766f7ce
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-18 15:45:51 +0600

    Added logs for valid URLs with timestamp

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -22,7 +22,13 @@ sap.ui.define([
 				// do something here, i.e. send logging date to the backend for analysis.
 				// telling what resource the user tried to access...
 				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
-			})
+			});
+			oRouter.attachRouteMatched(function (oEvent) {
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to back end
+				// in order to improve our app and  the user experience (Build-Measure-Learn cycle)
+				Log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
+			});
 		}
 
 	});
