d7a378886dbd5ca56544f5e1df7f0f028e922132
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-18 15:39:41 +0600

    Added log to console output invalid URL hashes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,28 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/demo/nav/controller/BaseController",
+	"sap/base/Log"
+], function (BaseController, Log) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.nav.controller.App", {
+	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 
 		onInit: function () {
+			/*
+				This is ONLY for being used within the tutorial. The default log level of the current
+				running environment may be higher than INFO, in order to see the debug info in the
+				console, the log level needs to be explicitly set to INFO here. But for application
+				development, the log level doesn't need to be set again in the code.
+			*/
+			Log.setLevel(Log.Level.INFO);
 
+			var oRouter = this.getRouter();
+
+			oRouter.attachBypassed(function (oEvent) {
+				var sHash = oEvent.getParameter("hash");
+				// do something here, i.e. send logging date to the backend for analysis.
+				// telling what resource the user tried to access...
+				Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+			})
 		}
 
 	});
